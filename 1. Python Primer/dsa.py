c9efb01f582cde79e985dc791c841f4e02298363
euphratica z <wilsonxyz@foxmail.com>
2017-12-18 19:58:35 -0500

    add details about function

--- 1. Python Primer/dsa.py
+++ 1. Python Primer/dsa.py
@@ -1,6 +1,9 @@
 # this is an implementation of data structures and algorithms
 
+############################################################
 # Part I Python Primer
+############################################################
+
 # computing the sum of a list of numbers
 # (build-in sum() function)
 total = 0
@@ -20,10 +23,22 @@ for j in range(len(data)):
     if data[j] > data[big_index]:
         big_index = j
 
-# add a function
-def count (data, target):
+############################################################
+# Define a Function
+#
+def count(data, target):
     n = 0
     for item in data:
         if item == target:  # found a match
             n += 1
     return n
+
+# objects that passing to and from a function are not copied
+
+# mutable parameter
+def scale(data, factor):
+    for j in range(len(data)):
+        data[j] *= factor
+
+
+
